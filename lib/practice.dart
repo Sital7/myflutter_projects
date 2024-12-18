1eac72ca2af49bc23999b2bf7d3bf40460e826b5
Sital khatri <129025536+Sital7@users.noreply.github.com>
2024-12-18 09:04:08 -0800

    Update practice.dart

--- lib/practice.dart
+++ lib/practice.dart
@@ -2,9 +2,45 @@ import 'dart:io';
 void main(){
   //you can take print or stdout for output and stdin for input
   print("welcome to dart");
+  // stdout.write("enter your name:");
+  //
+  // var name = stdin.readLineSync();
+  // print("welcome,$name");
 
-  stdout.write("enter your name:");
+  //declare variable without making the object variables can not be called
+  //nowadays we can create object without the use of new keyword also
+  var raman = new Human(); //or simply write Human()
 
-  var name = stdin.readLineSync();
-  print("welcome,$name");
+
+  //Declaration of variable int,string,boolean....etc
+  //if we want null value then we have to write int? a;
+  int a;
+
+  //Assign
+  a = 7;
+  print(a);
+
+  BigInt longvalue;
+  longvalue = BigInt.parse("3235643236237627636535653");
+  print(longvalue);
+
+  num precentage = 99.65;
+  bool isLogin = false;
+
+  isLogin = true;
+  
+
+  a = 9;
+  print(a);
+
+  //inline declaration
+  String name = "Raman";
+
+  name = "Ramanujan";
+
+}
+
+class Human{
+
+  Human();
 }
